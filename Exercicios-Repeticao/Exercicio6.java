84dacb788c40826023b497d2567f1431468d69de
Williamrsh <35608037+Williamrsh@users.noreply.github.com>
2021-12-10 16:29:56 -0300

    Update Exercicio6.java

--- Exercicios-Repeticao/Exercicio6.java
+++ Exercicios-Repeticao/Exercicio6.java
@@ -6,7 +6,8 @@ public class Exercicio6 {
 	
 	public static void main(String[] args) {
 		
-		int numero,media=0,cont=0,soma=0;
+		int numero,cont=0,soma=0;
+		float media=0;
 		Scanner read = new Scanner(System.in);
 		
 		System.out.println("Digite um numero: ");
@@ -26,8 +27,13 @@ public class Exercicio6 {
 			numero = read.nextInt();
 		
 			}while(numero !=0);
-			media=soma/cont;
-			System.out.println("A média dos numeros multiplos de 3 é "+media+".");
+			if(cont==0) {
+				System.out.println("NÃ£o tem numeros multiplos de 3");
+
+			}else {
+				media=soma/cont;
+				System.out.println("A mÃ©dia dos numeros multiplos de 3 Ã© "+media+".");
+			}
 		}
 	}
 }
